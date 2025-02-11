c46b6448578afd4ef4070132786d854dcb89affb
unknown <shubhgupta4102@gmail.com>
2025-02-11 12:43:19 +0530

    added typescript config

--- Models/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const connect = process.env.MONGO_URI;
-mongoose.connect(connect)
-  .then(() => console.log('Connected!'))
-  .catch((err) => console.error('Connection error:', err));
